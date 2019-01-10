e70884e578589bffdd8fb1e7ab220c2f04234968
O <O@DESKTOP-4JE2TMH>
2019-01-10 12:52:46 +0800

    1111

---
+++ src/main/java/com/t2/bean/Customer.java
@@ -0,0 +1,75 @@
+package com.t2.bean;
+
+import java.util.Date;
+
+public class Customer {
+    private Integer id;
+
+    private String name;
+
+    private String account;
+
+    private String password;
+
+    private String rid;
+
+    private Date interdate;
+
+    private String number;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account == null ? null : account.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getRid() {
+        return rid;
+    }
+
+    public void setRid(String rid) {
+        this.rid = rid == null ? null : rid.trim();
+    }
+
+    public Date getInterdate() {
+        return interdate;
+    }
+
+    public void setInterdate(Date interdate) {
+        this.interdate = interdate;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number == null ? null : number.trim();
+    }
+}
